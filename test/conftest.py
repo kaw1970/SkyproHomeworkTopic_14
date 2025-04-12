3afb714703b264c411d8a057068b2f44248c5cf5
Andrej Kudrjashow <kudrjashow@icloud.com>
2025-04-13 02:59:59 +0300

    черновик

---
+++ test/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+from src.Category import Category
+from src.Product import Product
+
+
+@pytest.fixture
+def first_product():
+    return Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+
+
+@pytest.fixture
+def first_category():
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        ["Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5]
+    )
